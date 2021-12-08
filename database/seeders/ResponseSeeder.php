8de8e84689c5587a209834182b47f1cef59ef44c
Jarom Rial <jarom.rial@gmail.com>
2021-12-07 20:42:25 -0700

    update ResponseSeeder

--- database/seeders/ResponseSeeder.php
+++ database/seeders/ResponseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Poll;
 use App\Models\Response;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ResponseSeeder extends Seeder
@@ -15,46 +16,21 @@ class ResponseSeeder extends Seeder
      */
     public function run()
     {
-        $ipAddresses = [
-            '133.58.59.122',
-            '206.47.196.207',
-            '3.131.46.243',
-            '61.43.170.224',
-            '26.80.139.228',
-            '36.69.61.61',
-            '107.190.253.52',
-            '45.236.65.26',
-            '169.180.174.36',
-            '237.89.49.86',
-            '137.0.216.117',
-            '43.21.34.149',
-            '145.68.46.19',
-            '181.32.160.168',
-            '211.28.213.238',
-            '145.124.62.240',
-            '200.135.131.97',
-            '239.174.33.109',
-            '239.242.248.130',
-            '139.13.23.249',
-        ];
-
-        $polls = Poll::get();
-
         $pollResponses = [
-            1 => [ 'Pizza', 'Tacos', 'Sushi', 'Falafel' ],
-            2 => [ 'Morgan Freeman', 'Steve Carrell', 'Samuel L. Jackson', 'Christopher Walken' ],
-            3 => [ 'Elated', 'Peaceful', 'Blessed', 'Happy' ],
-            4 => [ 'Facing In', 'Facing Out', 'On the floor' ],
-            5 => [ '1', '2', '3', '4' ],
-            6 => [ 'Smell', 'Hearing', 'Touch', 'Taste' ]
+            ['poll_id' => 1, 'responses' => [ 'Pizza', 'Tacos', 'Sushi', 'Falafel' ]],
+            ['poll_id' => 2, 'responses' => [ 'Morgan Freeman', 'Steve Carrell', 'Samuel L. Jackson', 'Christopher Walken' ]],
+            ['poll_id' => 3, 'responses' => [ 'Elated', 'Peaceful', 'Blessed', 'Happy' ]],
+            ['poll_id' => 4, 'responses' => [ 'Facing In', 'Facing Out', 'On the floor' ]],
+            ['poll_id' => 5, 'responses' => [ '1', '2', '3', '4' ]],
+            ['poll_id' => 6, 'responses' => [ 'Smell', 'Hearing', 'Touch', 'Taste' ]]
         ];
 
-        foreach ($ipAddresses as $address) {
-            foreach ($polls as $poll) {
-                $poll->responses()->save(new Response([
-                    'ip_address' => $address,
-                    'response' => $pollResponses[$poll->id][array_rand($pollResponses[$poll->id])]
-                ]));
+        foreach ($pollResponses as $poll) {
+            foreach ($poll['responses'] as $response) {
+                Response::create([
+                    'response' => $response,
+                    'poll_id' => $poll['poll_id'],
+                ]);
             }
         }
     }
